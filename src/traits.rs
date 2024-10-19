239baccc50261fe2af67c864dda3fd49a66a4714
Sondre <sbr075@uit.no>
2024-10-19 13:57:45 +0200

    feat!: rework from traits for string values for field value
    the previous solution could cause confusion as it would convert it to the "appropriate" type without explicitly telling the user. now when the user does FieldValue::from("true") it will remain a string. If the user wants to create a FieldValue::Boolean they'll have to use FieldValue::parse

--- src/traits.rs
+++ src/traits.rs
@@ -2,3 +2,9 @@ pub trait Format {
     fn escape(&self) -> Self;
     fn unescape(&self) -> Self;
 }
+
+pub trait Convert {
+    fn parse(s: &str) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+}
